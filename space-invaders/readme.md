9bd3ddc46eb44aceb8700022a537c3884364bb5c
ethereumvd <ethereum249@gmail.com>
2025-06-10 19:08:02 +0530

    removed spae invaders

--- space-invaders/readme.md
+++
@@ -1,3 +0,0 @@
-# Space Invaders
-
-Implementation of the classic Space Invaders Game in C++ using raylib
--- space-invaders/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-g++ src/main.cpp src/spaceship.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o bin/space-invaders
-
-./bin/space-invaders
